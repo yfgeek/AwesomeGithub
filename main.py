87b3520e89a4329e1aff431ea961431787d933fc
Ivan <yifanb@gmail.com>
2016-11-01 09:50:39 +0000

    english version

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 #-*- coding: utf-8 -*-
+# Code by Ivan
+# site:github.com/yfgeek
+
 import os
 import re
 import sys, urllib, urllib2
@@ -6,6 +9,8 @@ import time
 import cookielib
 
 class Github(object):
+
+    # Initializaiton Constructor
     # 初始化
     def __init__(self,name,password):
         self.user = name
@@ -16,6 +21,7 @@ class Github(object):
         getCookieUrl = "https://github.com/login"
         self.html = urllib2.urlopen(getCookieUrl).read()
 
+    # Browse the page to get Cookie
     # 模拟浏览器行为，得到Cookie
     def _getHeaders(self,referer):
         headers = {}
@@ -26,6 +32,8 @@ class Github(object):
         headers['Accept']='text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
         headers['Referer']= referer
         return headers
+
+    # sign up github
     # 方法登录网站
     def login(self):
         print "正在获取获取authenticity_token"
@@ -37,15 +45,19 @@ class Github(object):
         'login' : self.user,
         'password' : self.password
         }
-        #post数据登录
+        # post loginparams to login
+        # post数据登录
         req = urllib2.Request( 'https://github.com/session', urllib.urlencode(loginparams), headers=self._getHeaders('https://github.com/login'))
         resp = urllib2.urlopen(req)
         self.operate = self.opener.open(req)
         thePage = resp.read().decode("utf-8")
-        #查看登录结果
+        # print the result of login
+        # 查看登录结果
         print token
-        print "登陆成功"
+        print "Login Successful"
         return thePage
+
+    # Get the userToken
     # 获取Token
     def getUserToken(self,part):
         reg = re.compile('authenticity_token".*?value="(.*?)".*?>');
@@ -60,13 +72,15 @@ class Github(object):
         'utf8' : '%E2%9C%93',
         'authenticity_token' : token
         }
-        #post数据登录
+        # post loginparams to login
+        # post数据登录
         req = urllib2.Request( 'https://github.com/users/follow?target='+username, urllib.urlencode(loginparams), headers=self._getHeaders(''))
         resp = urllib2.urlopen(req)
         self.operate = self.opener.open(req)
         thePage = resp.read().decode("utf-8")
         return thePage
 
+    # get the userlist from a user who has a lot of followers
     # 获取复制用户的用户列表
     def listFollow(self,page,copyusername):
         url = "http://github.com/" + copyusername + "?page="+ str(page) +"&tab=following"
@@ -86,11 +100,16 @@ if sys.getdefaultencoding() != default_encoding:
     reload(sys)
     sys.setdefaultencoding(default_encoding)
 
+# new obejc parm1:username parm2:password
 #自动关注~ 用户名 密码
 gt = Github('username','password')
+
+# login
 # 登录
 gt.login()
+
+# range 1,5 is the page of follower page
 # range内是页数
 for i in range(1,5):
-    gt.listFollow(i,'yfgeek') #复制列表的人
+    gt.listFollow(i,'yfgeek') #复制列表的人 The person who you want to copy his follower to yours
 print "结束啦"
