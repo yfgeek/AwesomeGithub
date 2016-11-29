46cfaefedf5231173caa709d27ded43a9a280008
Ivan <yifanb@gmail.com>
2016-11-29 02:35:26 +0000

    fix bugs

--- GithubFollow.py
+++ GithubFollow.py
@@ -98,7 +98,7 @@ class GithubFollow(object):
             resp = urllib2.urlopen(req)
         except Exception, e:
             print "Retrying..."
-            self.__follow_user(self,token,username)
+            self.__follow_user(token,username)
             pass
         else:
             pass
